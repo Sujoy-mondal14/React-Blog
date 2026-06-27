8b237d6d2caac0930fb4e3be55a1e65fa395a594
Sujoy Mondal <sujoymondal1418852@gmail.com>
2026-06-27 17:08:01 +0530

    redux-toolkit configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import authService from '../src/appwrite/auth'
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
+  const [loading, setloading] = React.useState(false);
+  const dispatch = useDispatch()
+  React.useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false));
+  }, [])
 
 
-  return (
-    <>
+  return !loading ? (
+    <div className='min-h-screen dlex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
 
-    </>
-  )
+        <Header />
+
+        <main>
+          MAIN CONTENT {/* <Outlet/> */}
+        </main>
+
+        <Footer />
+
+      </div>
+
+    </div>
+  ) : null;
 }
 
 export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Header(){
+
+
+    return(
+        <div>
+            <h3>header</h3>
+        </div>
+    )
+}
